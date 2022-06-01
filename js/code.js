b7c89a0a0def2246faecd67e09213a0c0d121489
boxgames1 <boxgames1793@gmail.com>
2022-06-01 20:49:45 +0900

    Post's div
    + Post's auto-generating
    = Change loading text position

--- js/code.js
+++ js/code.js
@@ -1,16 +1,45 @@
 window.addEventListener('DOMContentLoaded', function() {
     const loading_p = document.getElementById('text-loading');
     const posts_div = document.getElementById('posts');
+    announcement_length = 10
     setTimeout(() => {
         loading_p.parentNode.removeChild(loading_p);
-        let i = 0;
-        while (i < 10) {
+        let i = announcement_length;
+        while (i > 0) {
+            // Some variables
             const new_post = document.createElement('div');
+            const new_post_title = document.createElement('a');
+            const new_post_description = document.createElement('p');
+            const new_post_image = document.createElement('img');
+            // Post Div
             new_post.setAttribute('class', 'div-post');
-            new_post.setAttribute('id', i.toString());
+            new_post.setAttribute('id', 'div-post-' + i.toString());
             new_post.innerHTML = "추가된 p태그";
+            // Post's title, desc, img
+            new_post_title.setAttribute('class', 'post-title');
+            new_post_title.setAttribute('href', 'https://goddot.kr');
+            new_post_title.setAttribute('target', '_self');
+            new_post_title.innerHTML = "제목" + i;
+            new_post_description.setAttribute('class', 'post-desc');
+            new_post_description.innerHTML = "설명";
+            new_post_image.setAttribute('class', 'post-img');
+            new_post_image.setAttribute('src', 'https://goddot.kr/goddot.jpg');
+            new_post_image.setAttribute('alt', '공지사항 ' + i + '번 이미지');
+            new_post_image.innerHTML = "";
+            // Add
             posts_div.appendChild(new_post);
-            i++;
+            new_post.appendChild(new_post_image);
+            new_post.appendChild(new_post_title);
+            new_post.appendChild(new_post_description);
+            i--;
         }
-    }, 1000);
+        i = announcement_length % 4;
+        while (i > 0) {
+            const new_empty_post = document.createElement('div');
+            new_empty_post.setAttribute('class', 'div-post-empty');
+            new_empty_post.innerHTML = "";
+            posts_div.appendChild(new_empty_post);
+            i--;
+        }
+    }, 1000); // Loading Delay(Default: 1000ms)
 });
